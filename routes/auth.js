27a5fbf56fb892322afc36b7f222f9e6465b72f1
vishalr4202 <vishalr4202@gmsil.com>
2023-10-17 23:43:52 +0530

    concurrent requests for the app with timecut off for entries in shyamendpoint

---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const UserController = require("../controllers/auth");
+const UserActionController = require("../controllers/userActions");
+const TrialController = require("../controllers/trialControls");
+const ShyamController = require("../controllers/shyamendpoint");
+const UserSchema = require("../validations/validations");
+const IsAuth = require("../middlewares/isAuth");
+const getKeys = require("../middlewares/isAuth");
+const router = express.Router();
+
+router.post(
+  "/signup",
+  UserSchema.userSignupSchema,
+  UserController.createSignup
+);
+
+router.post("/login", UserSchema.userLoginSchema, UserController.showLogin);
+
+router.get("/dashboard", IsAuth, UserActionController.getDashboard);
+router.post("/getAccessToken", IsAuth, UserActionController.getAccessToken);
+router.post("/placeTrade", UserActionController.placeFirstTrade);
+router.post("/getAllUsers", UserActionController.getAllUsers);
+router.post("/ticksData", TrialController.ticksData)
+router.post("/shyamendpoint", ShyamController.shyampoints)
+
+module.exports = router;
